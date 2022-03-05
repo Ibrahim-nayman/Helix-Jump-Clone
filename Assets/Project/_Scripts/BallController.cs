1cc2d62fd2f0f995432e1c6cb0ed5a69f67bfe77
İbrahim Nayman <84410079+Ibrahim-nayman@users.noreply.github.com>
2022-03-05 15:02:22 +0300

    Ball zıplama kodu eklendi

--- Assets/Project/_Scripts/BallController.cs
+++ Assets/Project/_Scripts/BallController.cs
@@ -1,8 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-   
+   private Rigidbody _rigidbodyBall;
+   public float Speed;
+
+   private void Start()
+   {
+      _rigidbodyBall = GetComponent<Rigidbody>();
+   }
+
+
+   private void Update()
+   {
+      if (Input.GetMouseButtonDown(0))
+      {
+         _rigidbodyBall.AddForce(new Vector3(0,Speed,0), ForceMode.Impulse);
+      }
+   }
 }
